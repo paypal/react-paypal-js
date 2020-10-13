c0543647e8fedc30a060c2c0fd875f13b92b4c32
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2020-10-13 11:08:30 -0500

    Linting error

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ export { default as PayPalMarks } from "./components/PayPalMarks";
 export { default as PayPalMessages } from "./components/PayPalMessages";
 export * from "./ScriptContext";
 
+const foo = 1;
+
 export { FUNDING } from "@paypal/sdk-constants/dist/module";
