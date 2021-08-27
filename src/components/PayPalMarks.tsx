26f18953cd534fd67201b4587b4e0fc260324f83
Arturo Riveron Borodovisina <aborodovisina@paypal.com>
2021-08-27 13:38:31 -0500

    fix(PayPalMarks): reload on fundingSOurce changes

--- src/components/PayPalMarks.tsx
+++ src/components/PayPalMarks.tsx
@@ -18,17 +18,13 @@ export interface PayPalMarksComponentProps extends PayPalMarksComponentOptions {
 The `<PayPalMarks />` component is used for conditionally rendering different payment options using radio buttons.
 The [Display PayPal Buttons with other Payment Methods guide](https://developer.paypal.com/docs/business/checkout/add-capabilities/buyer-experience/#display-paypal-buttons-with-other-payment-methods) describes this style of integration in detail.
 It relies on the `<PayPalScriptProvider />` parent component for managing state related to loading the JS SDK script.
-
 ```jsx
     <PayPalMarks />
 ```
-
 This component can also be configured to use a single funding source similar to the [standalone buttons](https://developer.paypal.com/docs/business/checkout/configure-payments/standalone-buttons/) approach.
 A `FUNDING` object is exported by this library which has a key for every available funding source option.
-
 ```jsx
     import { PayPalScriptProvider, PayPalMarks, FUNDING } from "@paypal/react-paypal-js";
-
     <PayPalScriptProvider options={{ "client-id": "test", components: "buttons,marks" }}>
         <PayPalMarks fundingSource={FUNDING.PAYPAL}/>
     </PayPalScriptProvider>
@@ -43,16 +39,48 @@ export const PayPalMarks: FunctionComponent<PayPalMarksComponentProps> = ({
     const mark = useRef<PayPalMarksComponent | null>(null);
     const [, setErrorState] = useState(null);
 
+    /**
+     * Remove any instance of the PayPal Mark from the DOM
+     */
+    const removeCurrentPayPalMark = () => {
+        if (
+            markContainerRef.current !== null &&
+            markContainerRef.current.firstChild != null
+        ) {
+            markContainerRef.current.removeChild(
+                markContainerRef.current.firstChild
+            );
+        }
+    };
+
+    /**
+     * Render PayPal Mark into the DOM
+     */
+    const renderPayPalMark = () => {
+        removeCurrentPayPalMark();
+        if (mark.current && markContainerRef.current) {
+            mark.current.render(markContainerRef.current).catch((err) => {
+                // component failed to render, possibly because it was closed or destroyed.
+                if (
+                    markContainerRef.current === null ||
+                    markContainerRef.current.children.length === 0
+                ) {
+                    // paypal marks container is no longer in the DOM, we can safely ignore the error
+                    return;
+                }
+                // paypal marks container is still in the DOM
+                setErrorState(() => {
+                    throw new Error(
+                        `Failed to render <PayPalMarks /> component. ${err}`
+                    );
+                });
+            });
+        }
+    };
+
     useEffect(() => {
         // verify the sdk script has successfully loaded
-        if (isResolved === false) {
-            return;
-        }
-
-        // don't rerender when already rendered
-        if (mark.current !== null) {
-            return;
-        }
+        if (isResolved === false) return;
 
         const paypalWindowNamespace = getPayPalWindowNamespace(
             options["data-namespace"]
@@ -72,30 +100,13 @@ export const PayPalMarks: FunctionComponent<PayPalMarksComponentProps> = ({
         mark.current = paypalWindowNamespace.Marks({ ...markProps });
 
         // only render the mark when eligible
-        if (mark.current.isEligible() === false) {
+        if (
+            mark.current.isEligible() === false ||
+            markContainerRef.current == null
+        )
             return;
-        }
 
-        if (markContainerRef.current === null) {
-            return;
-        }
-
-        mark.current.render(markContainerRef.current).catch((err) => {
-            // component failed to render, possibly because it was closed or destroyed.
-            if (
-                markContainerRef.current === null ||
-                markContainerRef.current.children.length === 0
-            ) {
-                // paypal marks container is no longer in the DOM, we can safely ignore the error
-                return;
-            }
-            // paypal marks container is still in the DOM
-            setErrorState(() => {
-                throw new Error(
-                    `Failed to render <PayPalMarks /> component. ${err}`
-                );
-            });
-        });
+        renderPayPalMark();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isResolved, markProps.fundingSource]);
 
