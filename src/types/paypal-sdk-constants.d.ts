b5c16dec168b6c9a23b9f9fa0007dc0647085152
Nate Bierdeman <nbierdeman@paypal.com>
2024-02-09 14:23:28 -0500

    fix(constants): alphabetize

--- src/types/paypal-sdk-constants.d.ts
+++ src/types/paypal-sdk-constants.d.ts
@@ -1,7 +1,7 @@
 declare module "@paypal/sdk-constants/dist/module" {
-    export const FUNDING: Record<string, string>;
-    export const SDK_QUERY_KEYS: Record<string, string>;
     export const CURRENCY: Record<string, string>;
-    export const SDK_SETTINGS: Record<string, string>;
+    export const FUNDING: Record<string, string>;
     export const SDK_INTEGRATION_SOURCES: Record<string, string>;
+    export const SDK_SETTINGS: Record<string, string>;
+    export const SDK_QUERY_KEYS: Record<string, string>;
 }
